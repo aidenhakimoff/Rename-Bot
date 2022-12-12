4b667085c5035649394e83f19a0042bfa1be8bd9
Aiden Hakimoff <88609829+aidenhakimoff@users.noreply.github.com>
2022-12-12 12:47:38 +0530

    Add files via upload

---
+++ helper/txt.py
@@ -0,0 +1,49 @@
+class mr(object):
+    PROGRESS_BAR = """\n
+🗂️ : {1} | {2}
+⏳️ : {0}%
+🚀 : {3}/s
+⏱️ : {4}
+ """
+
+    ABOUT_TXT = """
+╭───────────⍟
+├🤖 My Name : {}
+├👑 Developers : <a href=https://t.me/TitanBotUpdates>Titan Bots</a> 
+├👨‍💻 Programmer : <a href=https://t.me/Peaceluvr>Aiden Hakimoff</a>
+├📕 Library : <a href=https://github.com/pyrogram>Pyrogram</a>
+├✏️ Language : <a href=https://www.python.org>Python 3</a>
+├💾 Database : <a href=https://cloud.mongodb.com>MongoDB</a>
+├🌀 My Server : <a href=https://dashboard.heroku.com>Koyeb</a>
+├📊 Build Status : v1.0             
+╰───────────────⍟
+                                """
+    HELP_TXT = """
+🌌 <b><u>HOW TO SET THUMBNAIL</u></b>
+  
+•> /start a bot and send any picture to automatically set thumbnile.
+•> /delthumb use this command and delete your old thumbnile.
+•> /viewthumb use this command view your current thumbnile.
+
+📑 <b><u>HOW TO SET CUSTOM CAPTION</u></b>
+•> /set_caption - set a custom caption
+•> /see_caption - see your custom caption
+•> /del_caption - delete custom caption
+
+Example:- /set_caption 📕 File Name: {filename}
+💾 Size: {filesize}
+⏰ Duration: {duration}
+
+✏️ <b><u>HOW TO RENAME A FILE</u></b>
+•> send any file and click rename option and type new file name and \n send select [ document, video, audio ]👈 choice this.
+ℹ️ 𝗔𝗻𝘆 𝗢𝘁𝗵𝗲𝗿 𝗛𝗲𝗹𝗽 𝗖𝗼𝗻𝘁𝗮𝗰𝘁 :- <a href=https://t.me/LightningHelpDesk>Lightning Help Desk</a>
+"""
+
+    DEV_TXT = """
+<b><u>Special Thanks & Developers</b></u> 
+
+» 𝗦𝗢𝗨𝗥𝗖𝗘 𝗖𝗢𝗗𝗘 : <a href=https://github.com/aidenhakimoff/Rename-Bot>Rename Bot</a>
+
+• ❣️ <a href=https://github.com/aidenhakimoff>Aiden Hakimoff</a>
+
+"""
